26f0756aaa83e702c10f926c889a2fe119de22e6
iwant <iwant@DESKTOP-GB2RH00>
2021-09-30 21:34:08 +0600

    Добавьте файлы проекта.

---
+++ Matrix.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+class Matrix
+{
+private:
+    int** Item;
+    int Rows, Colums;
+public:
+    Matrix(int r, int c)
+    {
+    
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
